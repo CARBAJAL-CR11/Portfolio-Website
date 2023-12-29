f21a15bf3dff94954e4c78472587e65b3814f1fb
CARBAJAL-CR11 <cr4133668@gmail.com>
2023-12-29 00:26:23 -0600

    Mayus Correction :-1:

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import Navbar from './components/Navbar.jsx'
+import Navbar from './components/NavBar.jsx'
 import Home from './components/Home.jsx'
 import Aboutme from './components/Aboutme.jsx'
 import Skills from './components/Skills.jsx'
